f2270ca2ea7efa02443a86dd02e486f7f0f1eb97
Fabio Penna <ffmpenna@gmail.com>
2022-09-08 18:46:27 -0300

    requisito 4

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import CategoriesList from './CategoriesList';
 
 export default class Home extends React.Component {
   render() {
     return (
       <div>
+        <div>
+          <CategoriesList />
+        </div>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
