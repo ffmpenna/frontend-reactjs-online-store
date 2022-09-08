4e94e12a0139d6e2afcf6a5379758400f689f104
silvio@vinho <silviodrn52@gmail.comgit config --global user.name Silvio:wq:wqomit --global cleargit config --global user.email silviodrn52@gmail.com>
2022-09-08 19:30:44 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import Home from './components/Home';
+import ShoppingCart from './components/ShoppingCart';
 // import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={ Home } />
-
+        <Route exact path="/" component={ Home } />
+        <Route path="/shoppingcart" component={ ShoppingCart } />
       </Switch>
+      <Link data-testid="shopping-cart-button" to="/shoppingcart">Carrinho</Link>
     </BrowserRouter>
   );
 }
---
+++ src/components/ShoppingCart.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import CartEmpty from './CartEmpty';
+
+export default class ShoppingCart extends Component {
+  render() {
+    return (
+      <div>
+        <CartEmpty />
+      </div>
+    );
+  }
+}
