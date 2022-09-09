3f42305a713c96a8d676ff5a0ce0bca6d6c6dbab
Fabio Penna <ffmpenna@gmail.com>
2022-09-08 21:56:36 -0300

    requisito 5

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class ProductCard extends React.Component {
+  render() {
+    const { thumbnail, title, price, productId } = this.props;
+    return (
+      <div
+        productId={ productId }
+        data-testid="product"
+      >
+        <p>{title}</p>
+        <img src={ thumbnail } alt={ title } />
+        <p>{price}</p>
+      </div>
+    );
+  }
+}
+
+ProductCard.propTypes = {
+  thumbnail: PropTypes.string,
+  title: PropTypes.string,
+  price: PropTypes.string,
+  productId: PropTypes.string,
+}.isRequired;
